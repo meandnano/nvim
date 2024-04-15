7c9bec6d3ab8332da795109231ec9ff3b8cd9a7d
mike.art <meandnano@gmail.com>
2024-04-15 15:29:12 +0200

    Make selection to preserve

--- lua/meandnano/lazy/lsp.lua
+++ lua/meandnano/lazy/lsp.lua
@@ -42,7 +42,7 @@ return {
 				'lua_ls',
 			},
 			handlers = {
-				function(server_name)         -- default handler (optional)
+				function(server_name) -- default handler (optional)
 					require("lspconfig")[server_name].setup {
 						capabilities = capabilities
 					}
@@ -62,6 +62,10 @@ return {
 						}
 					}
 				end,
+
+				["hydra_lsp"] = function()
+
+				end,
 			}
 		})
 
@@ -70,7 +74,7 @@ return {
 		cmp.setup({
 			snippet = {
 				expand = function(args)
-					require('luasnip').lsp_expand(args.body)           -- For `luasnip` users.
+					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
@@ -81,7 +85,7 @@ return {
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },         -- For luasnip users.
+				{ name = 'luasnip' }, -- For luasnip users.
 			}, {
 				{ name = 'buffer' },
 			})
--- lua/meandnano/remap.lua
+++ lua/meandnano/remap.lua
@@ -1,17 +1,20 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>fu", vim.cmd.Ex, {desc = "go to directory"})
+vim.keymap.set("n", "<leader>fu", vim.cmd.Ex, { desc = "go to directory" })
 
 -- preserve buffer on paste (greates remap ever!)
-vim.keymap.set("x", "<leader>p", "\"_dP", {desc = "preserve buffer on paste"})
+vim.keymap.set("x", "<leader>p", "\"_dP", { desc = "preserve buffer on paste" })
 
 -- yank into the system buffer (next greatest remap)
-vim.keymap.set("n", "<leader>y", "\"+y", {desc = "yank to clipboard"})
-vim.keymap.set("n", "<leader>Y", "\"+Y", {desc = "yank to clipboard"})
-vim.keymap.set("v", "<leader>y", "\"+y", {desc = "yank to clipboard"})
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = "yank to clipboard" })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "yank to clipboard" })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "yank to clipboard" })
 
 -- move selection up/down
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- up
+
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
 
 -- while scrolling through search results, keep the cursom in the middle
 vim.keymap.set("n", "n", "nzzzv")
@@ -20,16 +23,17 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "Q", "<nop>")
 
 -- start replacing the current wod
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc = "replace current word"})
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "replace current word" })
 
 vim.keymap.set("n", "<leader>cfb", function()
 	vim.lsp.buf.format()
-end, {desc = "format buffer"})
+end, { desc = "format buffer" })
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", {desc = "quickfix next"})
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", {desc = "quickfix prev"})
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "quickfix next" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "quickfix prev" })
 
 vim.keymap.set("n", "<leader>bb", function()
 	print(vim.fn.FugitiveHead(8))
