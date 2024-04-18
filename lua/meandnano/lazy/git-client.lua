fef981f7857f0684c021cb6d8dc90849258b747f
mike.art <meandnano@gmail.com>
2024-04-18 15:26:00 +0200

    Switch from fugitive to neogit

---
+++ lua/meandnano/lazy/git-client.lua
@@ -0,0 +1,24 @@
+-- return {
+-- 	"tpope/vim-fugitive",
+-- 	config = function()
+-- 		vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
+-- 	end
+-- }
+
+return {
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+
+		-- Only one of these is needed, not both.
+		"nvim-telescope/telescope.nvim", -- optional
+		"ibhagwan/fzf-lua",            -- optional
+	},
+	keys = {
+		{ "<leader>gg", "<cmd>Neogit<CR>" }
+	},
+	config = function()
+		require("neogit").setup {}
+	end
+}
